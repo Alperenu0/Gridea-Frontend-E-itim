73cbc3affe774a6f6aaf150c99a15d1c1101ce13
Alperenu0 <148974839+Alperenu0@users.noreply.github.com>
2024-03-22 23:08:23 +0300

    js ödevi yapıldı

---
+++ Homework8 Alperen Ucar/homework8.js
@@ -0,0 +1,80 @@
+// Alperen Uçar
+var a = "BILTEK";
+console.log(a);
+
+const b= "AYBU";
+console.log(b);
+
+var random="dskljdkslj";
+console.log(random.length)
+
+var num = 2;
+console.log(num+5)
+
+// ---------------------------
+
+var num1 = 24;
+var num2 = 6;
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+
+var username="Alperen ";
+var usersurname="Ucar";
+
+console.log(username+usersurname);
+
+var value1=14;
+var value2="14";
+
+console.log(value1==value2);
+console.log(value1===value2);
+// -----------------------------
+var numb=12;
+
+if(numb > 10){
+    console.log(numb +" sayisi 10 sayisindan buyuktur...");
+}
+else if(numb==10){
+    console.log(numb +" sayisi 10 sayisina esittir...");
+}
+else{
+    console.log(numb+" sayisi 10 sayisindan kucuktur...");
+}
+// ------------------------------
+var rand="aaaa";
+
+if(rand.length==5){
+    console.log(rand+" kelimesinin uzunlugu 5 e eşittir...");
+}
+
+else{
+        console.log(rand+" kelimesinin uzunlugu 5 ten farklidir...");
+
+}
+
+// -------------------------------
+
+
+var number1= "2";
+
+
+switch(number1){
+    case "1":
+        console.log(number1+" sayisi 1 dir");
+        break;
+
+    case "2":
+         console.log(number1+" sayisi 2 dir");
+         break;    
+
+    case "3":
+        console.log(number1+" sayisi 3 tür");
+        break;    
+        
+    
+
+      
+}
